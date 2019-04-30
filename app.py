a4962031d8217fb6e3461365d5632a4231482d98
Samuel <otacon100@hotmail.com>
2019-04-30 22:13:27 +0100

    Textbox added, page looks nicer

--- app.py
+++ app.py
@@ -12,14 +12,11 @@ def add_messages(username, message):
     Add messages to the 'messages' list
     """
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
+    messages_dict = {"timestamp": now,
+                     "from": username,
+                     "message": message}
 
-
-def get_all_messages():
-    """
-    Get all the message and separate using a break
-    """
-    return "<br>".join(messages)
+    messages.append(messages_dict)
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -36,12 +33,18 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/<username>')
+@app.route('/<username>', methods=["GET", "POST"])
 def user(username):
     """
     Display chat messages
     """
-    return "<h1>Welcome, {0}</h1>  {1} ".format(username, get_all_messages())
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
+    return render_template("chat.html", username=username, chat_messages=messages)
 
 
 @app.route('/<username>/<message>')
